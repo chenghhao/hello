b0c99c320c2b01999f9029bb33f9dd664c0b4b19
Mark Rose <Mark.Rose@nasa.gov>
2012-06-29 15:25:14 -0700

    Modified hello message.

--- src/main/java/gov/nasa/arc/hello/Hello.java
+++ src/main/java/gov/nasa/arc/hello/Hello.java
@@ -3,7 +3,7 @@ package gov.nasa.arc.hello;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Goodbye, cruel world!");
     }
 
 }
